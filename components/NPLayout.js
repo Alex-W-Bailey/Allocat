06ce03ebb70445f048e3128bddc3e45b3f616247
Monica <monica.dixon8@gmail.com>
2020-02-10 16:09:06 -0600

    added forms for tasks and collaborators

--- components/NPLayout.js
+++ components/NPLayout.js
@@ -1,8 +1,12 @@
 // New Project (forms) Layout
+const NPstyles = {
+  border: 2,
+  borderradius: 25
+};
 
 const NPLayout = props => {
   return (
-    <div className='container mx-auto row'>
+    <div style={NPstyles} className='container mx-auto row'>
       <div className='col-md-10 mx-auto'>{props.children}</div>
     </div>
   );
