8075c39f75a0a4ac4341120fa0ab488e727f205f
Danielle Burrage <burragedanielle@gmail.com>
2020-02-10 16:08:27 -0600

    npcard // small add

---
+++ components/NPCard.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+const NPCard = props => {
+
+    return (
+        <div>
+            <div className='col-sm-4'>
+                <div className='card-body m-2'>
+                    <h5 className="card-title">Add New Project</h5>
+                    <p className="card-text">Ready to get started on a new project? Add a new project to your projects list.</p>
+                    <Link>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NPCard;
