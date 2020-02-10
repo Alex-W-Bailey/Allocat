4de144ae861c90835da0b9de692cabcce8f00705
Alex Bailey <alexwbailey7@gmail.com>
2020-02-09 20:03:58 -0600

    user password is now encrypted

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt")
 const db = require("../models");
 
 module.exports = app => {
@@ -7,8 +8,34 @@ module.exports = app => {
     
     // Create a new example
     app.post("/api/newUser", (req, res) => {
-        db.User.create(req.body).then((dbUser) => {
-            res.json(dbUser);
-        });
+        db.User.findOne({
+            where: {
+                Email: req.body.Email
+            }
+        }).then(function(user) {
+            if(user) {
+                return done(null, false, {
+                    message: 'That email is already taken'
+                });
+            }
+            else {
+                const newUser = {
+                    Email: req.body.Email,
+                    FullName: req.body.FullName,
+                    Password: req.body.Password
+                }
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.Password, salt, (err, hash) => {
+                        if(err) throw err;
+                        newUser.Password = hash;
+
+                        db.User.create(newUser).then((dbUser) => {
+                            res.json(dbUser);
+                        }); 
+                    })
+                })     
+            }
+        })
     });
 };
