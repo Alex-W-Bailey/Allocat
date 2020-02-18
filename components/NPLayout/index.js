40536ccd8a7af21c81cfda6ce14e3fc4fb99116e
Monica <monica.dixon8@gmail.com>
2020-02-18 10:31:14 -0600

    made variables for colors and moved imports

--- components/NPLayout/index.js
+++ components/NPLayout/index.js
@@ -1,13 +1,13 @@
-// New Project (forms) Layout
-const NPstyles = {
-  border: 2,
-  borderradius: 25
-};
+// // New Project (forms) Layout
+// const NPstyles = {
+//   border: 2,
+//   borderradius: 25
+// };
 
 const NPLayout = props => {
   return (
-    <div style={NPstyles} className='container mx-auto row'>
-      <div className='col-md-10 mx-auto'>{props.children}</div>
+    <div className='container mx-auto row'>
+      <div className='np-container col-md-10'>{props.children}</div>
     </div>
   );
 };
