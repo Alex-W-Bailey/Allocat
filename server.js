081b3e3d259c74909a6d83cc724bb00039ddb867
xKingAlex <41297819+xKingAlex@users.noreply.github.com>
2020-02-22 13:00:17 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cors = require('cors');
 const session = require('express-session');
-const next = require( 'next' );
+const next = require('next');
 
 const db = require("./models");
 const passport = require("passport");
