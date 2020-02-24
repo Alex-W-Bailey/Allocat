7376793741c1d7ee4db10fdb0ba2a777013e70b3
Monica <monica.dixon8@gmail.com>
2020-02-23 19:39:38 -0600

    success screen

--- components/CreatedProject/index.js
+++ components/CreatedProject/index.js
@@ -1,6 +1,6 @@
-import Nav from "../Nav/index"
+import Nav from "../Nav/index";
 import NPLayout from "../NPLayout/index";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 const NPForm = props => {
@@ -9,16 +9,16 @@ const NPForm = props => {
       <Nav pageTitle={""} />
       <NPLayout>
         <div className='row mt-5'>
-          <h1>Success!</h1>
+          <h1 className='blue-text'>Success!</h1>
           <br />
-          <button onClick={() => props.handleRedirectToProjects()}>
-            View All Projects
-          </button>
-          <Link href="/project/[id]" as={`/project/${props.projectId}`}>
-            <button>
-              View project
+          <div className='row'>
+            <button onClick={() => props.handleRedirectToProjects()}>
+              View All Projects
             </button>
-          </Link>
+            <Link href='/project/[id]' as={`/project/${props.projectId}`}>
+              <button>View project</button>
+            </Link>
+          </div>
         </div>
       </NPLayout>
     </div>
